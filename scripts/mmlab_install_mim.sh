ab5f7e60a3ace080b3c77d1ef961a051a77a26b4
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2026-05-17 23:38:36 -0500

    fixed mim script

--- scripts/mmlab_install_mim.sh
+++ scripts/mmlab_install_mim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mim install mmengine
-mim install mmcv==2.1.0
-mim install mmpretrain==1.2.0
-mim install mmdet==3.2.0
-mim install mmpose==1.3.2
+uv run mim install mmengine
+uv run mim install mmcv==2.1.0
+uv run mim install mmpretrain==1.2.0
+uv run mim install mmdet==3.2.0
+uv run mim install mmpose==1.3.2
